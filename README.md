d36826dd9b0529446528d2dbf6015de11f6ca32a
jammygrams <jmahbubani@gmail.com>
2023-02-02 22:12:01 +0800

    make pretty

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Todo
 **features**
-* Switch to Next.js -> api runs on server?
-* Data persists in firestore? need single user / login?
-* Come up with design v2
+* Data persists in firestore? need single user / login? <- do this, using course materials
+* Come up with design v2 - tough, ask kaiden
 * teams
 * multiplayer
 
 **minor**
+* display tables used in query in summary
 * format user input into proper sql
 * loading symbol while waiting for response?
 
--- components/Accordion.js
+++ components/Accordion.js
@@ -4,6 +4,7 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from '@mui/icons-material/Add';
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import dynamic from "next/dynamic";
@@ -106,7 +107,7 @@ export function Accordions() {
   function AddAccordionButton({ onClick }) {
     return (
       <Button variant="text" onClick={onClick}>
-        Add Row
+        <AddIcon />
       </Button>
     );
   }
