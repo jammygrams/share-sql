df67c349b7bec72c0cc292b0dffe53ae2bf4fb13
jammygrams <jmahbubani@gmail.com>
2023-02-03 19:16:38 +0800

    only write tutorial doc when first log in works

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 ## Todo
 **features**
-* Data persists in firestore? need single user / login? <- do this, using course materials
+* Data persists in firestore
+    * anonymous firestore login in index.js (done)
+    * when login, create doc in users with user id (done)
+    * we pass user info to context in _app.js, so we can query user documents (done)
+        * TODO: will this actually work for new user?
+    * for user, if no code data, create tutorial post?
+    * later
+        * load all user posts and display them
+        * everytime we edit summary or code, update doc? TODO: every update write to firestore? debounce? at click?
 * Come up with design v2 - tough, ask kaiden
 * teams
 * multiplayer
 
-**minor**
+**lower priority**
+* convert anonymous login to google login
 * display tables used in query in summary
 * format user input into proper sql
+* if user has already written something, incorporate into generation? maybe not?
 * loading symbol while waiting for response?
 
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,61 @@
 import { ChildAccordion, Accordions } from "../components/Accordion";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { signInAnonymously } from "firebase/auth";
-import { auth, db } from '../lib/firebase'
-import { doc, setDoc } from "firebase/firestore"; 
-import CssBaseline from '@mui/material/CssBaseline';
+import { auth, db } from "../lib/firebase";
+import { serverTimestamp } from "firebase/firestore";
+import { collection, doc, setDoc, addDoc } from "firebase/firestore";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
+
+const tutorialDoc = {
+  content: "SELECT this, that FROM table1 WHERE that == '9'",
+  summary: "Return all this where that equals 9",
+  createdAt: serverTimestamp(),
+  updatedAt: serverTimestamp(),
+};
 
 const TutorialCode = "SELECT this, that FROM table1 WHERE that == '9'";
 const TutorialSummary = "Return all this where that equals 9";
 
+function isNewUser(user) {
+  const threshold = 500;
+  return (
+    Math.abs(user.metadata.lastSignInTime - user.metadata.creationTime) >
+    threshold
+  );
+}
+
 async function createUser() {
-  const signInResult = await signInAnonymously(auth)
+  const signInResult = await signInAnonymously(auth);
   const userUid = signInResult.user.uid; // The UID of the user.
-  const userDoc = {
-    username: ""
-  }
+  const userDoc = {};
   console.log("User ID: ", userUid);
   try {
     await setDoc(doc(db, "users", userUid), userDoc);
+    if (isNewUser(signInResult.user)) {
+      const docId = await addDoc(
+        collection(db, "users", userUid, "code"),
+        tutorialDoc
+      );
+      console.log("Added tutorial doc id: ", docId.id);
+    }
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
 
 export default function Home() {
-  createUser()
+  createUser();
 
   return (
     <>
       <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
+        <CssBaseline />
         <ChildAccordion
           id={1}
           initialCode={TutorialCode}
