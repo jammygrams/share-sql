bb4495223dac5e263e6c716fac1e3572541d7d58
jammygrams <jmahbubani@gmail.com>
2023-02-03 19:22:23 +0800

    refactor firebase to lib

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
     * when login, create doc in users with user id (done)
     * we pass user info to context in _app.js, so we can query user documents (done)
         * TODO: will this actually work for new user?
-    * for user, if no code data, create tutorial post?
-    * later
-        * load all user posts and display them
-        * everytime we edit summary or code, update doc? TODO: every update write to firestore? debounce? at click?
+    * for user, if no code data, create tutorial post (done)
+    * load user posts and display them
+    * everytime we edit summary or code, update doc?
+        * debounce?
 * Come up with design v2 - tough, ask kaiden
 * teams
 * multiplayer
@@ -18,7 +18,7 @@
 **lower priority**
 * convert anonymous login to google login
 * display tables used in query in summary
-* format user input into proper sql
+* format user input sql into proper sql, will be important for gpt
 * if user has already written something, incorporate into generation? maybe not?
 * loading symbol while waiting for response?
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import { ChildAccordion, Accordions } from "../components/Accordion";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { signInAnonymously } from "firebase/auth";
-import { auth, db } from "../lib/firebase";
+import { createUser } from "../lib/firebase";
 import { serverTimestamp } from "firebase/firestore";
-import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const darkTheme = createTheme({
@@ -22,35 +20,8 @@ const tutorialDoc = {
 const TutorialCode = "SELECT this, that FROM table1 WHERE that == '9'";
 const TutorialSummary = "Return all this where that equals 9";
 
-function isNewUser(user) {
-  const threshold = 500;
-  return (
-    Math.abs(user.metadata.lastSignInTime - user.metadata.creationTime) >
-    threshold
-  );
-}
-
-async function createUser() {
-  const signInResult = await signInAnonymously(auth);
-  const userUid = signInResult.user.uid; // The UID of the user.
-  const userDoc = {};
-  console.log("User ID: ", userUid);
-  try {
-    await setDoc(doc(db, "users", userUid), userDoc);
-    if (isNewUser(signInResult.user)) {
-      const docId = await addDoc(
-        collection(db, "users", userUid, "code"),
-        tutorialDoc
-      );
-      console.log("Added tutorial doc id: ", docId.id);
-    }
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-}
-
 export default function Home() {
-  createUser();
+  createUser(tutorialDoc);
 
   return (
     <>
