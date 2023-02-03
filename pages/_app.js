c34237d8ba1738515ef52985cba0fa1eacfeeeac
jammygrams <jmahbubani@gmail.com>
2023-02-03 17:31:00 +0800

    anon login and write user to firestore working

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,26 @@
+import { UserContext } from "@/lib/context";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // const auth = getAuth();
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     // User is signed in, see docs for a list of available properties
+  //     // https://firebase.google.com/docs/reference/js/firebase.User
+  //     const uid = user.uid;
+  //     // ...
+  //   } else {
+  //     // User is signed out
+  //     // ...
+  //     console.log("user signed out!")
+  //   }
+  // });
+// value={{ uid }}
+
+  return (
+    <UserContext.Provider> 
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { ChildAccordion, Accordions } from "../components/Accordion";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { signInAnonymously } from "firebase/auth";
+import { auth, firestore } from '../lib/firebase'
+import { collection, doc, setDoc } from "firebase/firestore"; 
 import CssBaseline from '@mui/material/CssBaseline';
 
 const darkTheme = createTheme({
@@ -11,7 +14,23 @@ const darkTheme = createTheme({
 const TutorialCode = "SELECT this, that FROM table1 WHERE that == '9'";
 const TutorialSummary = "Return all this where that equals 9";
 
+async function createUser() {
+  const signInResult = await signInAnonymously(auth)
+  const userUid = signInResult.user.uid; // The UID of the user.
+  const userDoc = {
+    username: ""
+  }
+  console.log("User ID: ", userUid);
+  try {
+    await setDoc(doc(firestore, "users", userUid), userDoc);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
 export default function Home() {
+  createUser()
+
   return (
     <>
       <ThemeProvider theme={darkTheme}>
