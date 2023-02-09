3c2169c309075a17b28a3c6ec753d250bebe8744
jammygrams <jmahbubani@gmail.com>
2023-02-09 11:19:38 +0800

    styling

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,23 +1,56 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import { styled } from "@mui/material/styles";
+// import Accordion from "@mui/material/Accordion";
+// import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordion from "@mui/material/Accordion";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import dynamic from "next/dynamic";
 import { Typography } from "@mui/material";
-import { queryGPT } from  "@/lib/openai"
+import { queryGPT } from "@/lib/openai";
 import { UserContext } from "@/lib/context";
 
 // Need to import CodeEditor with no server side rendering
 // ref: https://github.com/securingsincity/react-ace/issues/1044
 const CodeEditor = dynamic(() => import("./CodeEditor"), { ssr: false });
 
+// These have to be at top level, including them in ChildAccordion
+//   means they are recreated everytime ChildAccordion rerenders, so no animation
+const Accordion = styled((props) => <MuiAccordion disableGutters {...props} />)(
+  ({ theme }) => ({
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+  })
+);
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
 export function ChildAccordion({ index }) {
-  const { user, documents, setDocuments, isLoading, setLoading } = React.useContext(UserContext);
+  const { user, documents, setDocuments, isLoading, setLoading } =
+    React.useContext(UserContext);
   // make sure first accordion is expanded at start
   var isExpanded = null;
   index === 0 ? (isExpanded = true) : (isExpanded = false);
@@ -87,14 +120,13 @@ export function ChildAccordion({ index }) {
     <div>
       <Accordion expanded={expanded}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
+          aria-controls="panel1d-content"
+          id="panel1d-header"
           onClick={handleSummaryClick}
         >
-          <Grid container spacing={1}>
-            <Grid item xs={0.5}>
-              <Typography>{index + 1}</Typography>
+          <Grid container spacing={1} alignItems="center">
+            <Grid item xs={0.5} style={{ textAlign: "center" }}>
+              <Typography variant="button">{`${index + 1}.`}</Typography>
             </Grid>
             <Grid item xs={11.5}>
               <TextField
