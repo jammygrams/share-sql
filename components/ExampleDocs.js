a2658fdf1b25c4b78377d2aacbbe026dd12a2f69
jammygrams <jmahbubani@gmail.com>
2023-02-09 19:40:12 +0800

    fix bug with new user docs

--- components/ExampleDocs.js
+++ components/ExampleDocs.js
@@ -1,24 +1,25 @@
 import { serverTimestamp } from "firebase/firestore";
 
 export const tutorialDoc = {
-  // created in firebase so don't need id structure
+  id: null,
+  data: {
     content: `
-    SELECT DISTINCT ON (customer)
-      id, customer, total
-    FROM purchases
-    ORDER BY customer, total DESC, id;`,
+      SELECT DISTINCT ON (customer)
+        id, customer, total
+      FROM purchases
+      ORDER BY customer, total DESC, id;`,
     summary: "Select the highest purchase total for each customer.",
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp(),
-  };
-  
+  },
+};
+
 export const newDoc = {
-  // created in app so need id structure
-    id: null,
-    data: {
-      content: "",
-      summary: "",
-      createdAt: serverTimestamp(),
-      updatedAt: serverTimestamp(),
-    }
-  };
+  id: null,
+  data: {
+    content: "",
+    summary: "",
+    createdAt: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import ButtonAppBar from "@/components/Appbar";
 import { UserContext } from "../lib/context";
-import { createUserAndDoc, db, auth } from "../lib/firebase";
-import { tutorialDoc } from "../components/ExampleDocs";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
-import { onAuthStateChanged } from "firebase/auth";
+import { setUpUserAndDocs } from "../lib/firebase";
 import React, { useEffect } from "react";
 
 const darkTheme = createTheme({
@@ -19,42 +16,20 @@ export default function App({ Component, pageProps }) {
   const [documents, setDocuments] = React.useState([]);
   const [isLoading, setLoading] = React.useState(false);
 
-  // TODO: move this to firebase.js so all that code is together?
   useEffect(() => {
-    createUserAndDoc(tutorialDoc);
-    onAuthStateChanged(auth, (authUser) => {
-      if (authUser) {
-        setUser(authUser);
-        getDocs(
-          query(
-            collection(db, "users", authUser.uid, "code"),
-            orderBy("createdAt")
-          )
-        ).then((querySnapshot) => {
-          setDocuments(
-            querySnapshot.docs.map((doc) => ({
-              id: doc.id,
-              data: doc.data(),
-            }))
-          );
-        });
-      } else {
-        setUser(null);
-      }
-    });
+    setUpUserAndDocs({ documents, setDocuments, setUser });
   }, []);
 
   return (
     <>
-      <UserContext.Provider value={{ user, documents, setDocuments, isLoading, setLoading }}>
+      <UserContext.Provider
+        value={{ user, documents, setDocuments, isLoading, setLoading }}
+      >
         <ThemeProvider theme={darkTheme}>
           <CssBaseline />
           <ButtonAppBar />
           <Component
             {...pageProps}
-            // documents={documents}
-            // user={user}
-            // setDocuments={setDocuments}
           />
         </ThemeProvider>
       </UserContext.Provider>
